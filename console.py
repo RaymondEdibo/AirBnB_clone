9c844cc3885f9f7b02badec425e76aeb5f0a9876
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-10-14 18:41:47 +0100

    Update console.py

--- console.py
+++ console.py
@@ -98,5 +98,47 @@ class HBNBCommand(cmd.Cmd):
                 setattr(obj, args[2], args[3])
                 storage.save()
 
+    def count(self, arg):
+        """
+        Prints number of instances
+            usage: count <class_name>
+        """
+        args = shlex.split(arg)
+        models.storage.reload()
+        if len(args) < 1:
+            print(self.errors["missingClass"])
+        elif args[0] in self.classes:
+            instances = str(models.storage.all().keys())
+            print(instances.count(args[0]))
+        else:
+            print(self.errors["wrongClass"])
+
+    def default(self, line):
+        """Handle default behaviour"""
+        funcs = {"all": self.do_all, "count": self.count, "show": self.do_show,
+                 "destroy": self.do_destroy, "update": self.do_update}
+        cmd = line.split('.', 1)
+        class_name = cmd[0]
+        args = [None]
+        if len(cmd) > 1:
+            args = cmd[1].strip("()").split('(')
+        if args[0] in funcs:
+            func = funcs[args[0]]
+            params = class_name + ' '
+            if len(args) > 1:
+                if args[0] == "update" and args[1][-1] == '}':
+                    str_dict = args[1].split(' ', 1)[1]
+                    upd_dict = ast.literal_eval(str_dict)
+                    params += args[1].split(',', 1)[0] + ' '
+                    for k, v in upd_dict.items():
+                        fparams = '{} "{}" "{}"'.format(params, str(k), str(v))
+                        func(fparams)
+                    return
+                else:
+                    params += args[1].replace(',', '')
+            func(params)
+        else:
+            print("*** Unknown syntax: {}".format(line))
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
