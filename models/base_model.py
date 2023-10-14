fbc14f0906b90bad4646185d702ba4e808575bb8
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-10-14 19:28:01 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Base Model module which;
+"""Base Model module.
 
-defines all common attributes/methods
-for other classes
+Contains BaseModel class which is
+"base" of all other classes.
 """
 import uuid
 from datetime import datetime
@@ -10,22 +10,23 @@ import models
 
 
 class BaseModel():
-    """Base Model module.
-    
-    Public instance attributes:
+    """Base Model module
+
+    used for all common attributes and methods management for all other classes.
+
+    Attributes:
         id (str): a uuid when an instance is created.
         created_at (datetime): the current datetime when an instance is created.
         updated_at (datetime): the current datetime when an instance is created and it will be updated.
     """
 
     def __init__(self, *args, **kwargs):
-        """constructor of a BaseModel.
+        """Initializes BaseModel.
 
         Args:
-            *args: won’t be used
-            kwargs (dict): dictionary
+            *args: won’t be used.
+            **kwargs (dict): dictionary.
         """
-        
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
         if kwargs is not None and kwargs != {}:
@@ -36,10 +37,11 @@ class BaseModel():
                     setattr(self, k, v)
         else:
             models.storage.new(self)
-    
+
     def __str__(self):
-        """should print: [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """should print: [<class name>] (<self.id>) <self.__dict__>."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at with the current datetime."""
@@ -47,7 +49,7 @@ class BaseModel():
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all keys/values of __dict__ of the instance."""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
