975aa33cb471291c3d381d407462eb568380f97a
Raymond <97799002+RaymondEdibo@users.noreply.github.com>
2023-10-14 19:16:24 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,20 +19,20 @@ class TestConsoleDocs(unittest.TestCase):
         result = pep8s.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_console_module_docstring(self):
-        """console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
-
+        
     def test_pep8_conformance_test_console(self):
         """tests/test_console.py conformity to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+    
+    def test_console_module_docstring(self):
+        """console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
         
     def test_HBNBCommand_class_docstring(self):
         """HBNBCommand class docstring"""
